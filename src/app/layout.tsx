0c71a1b0c291d36b6e24a34e15fc533d07b3077a
cdiaz2024 <cgd6055@gmail.com>
2025-11-18 13:03:52 -0500

    added ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Quicksand } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import StructuredData from "../components/StructuredData";
 
@@ -80,6 +81,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-X6V9PJ3ECJ"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-X6V9PJ3ECJ');
+          `}
+        </Script>
+      </head>
       <body
         className={`${quicksand.variable} antialiased`}
       >
